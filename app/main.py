d65938c14ba538daf74895f57077af14e85681c7
Kml <208549849+kimo667@users.noreply.github.com>
2026-09-14 16:50:42 +0200

    ajout route token login et generation jwt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.core.security import supabase
+from app.core.security import supabase, SECRET_KEY, ALGORITHM
 from app.core.metabolism import UserProfile, calculer_profil_complet
 
 app = FastAPI(title="Smart Fridge & Nutrition Coach")
@@ -29,3 +32,18 @@ def register_user(form_data: OAuth2PasswordRequestForm = Depends()):
     }
     new_user = supabase.table("users").insert(new_user_data).execute()
     return {"message": "User registered successfully"}
+
+
+@app.post("/token")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    response = supabase.table("users").select("*").eq("username", form_data.username).execute()
+    users_list = response.data
+
+    if not users_list or users_list[0]["password"] != form_data.password:
+        raise HTTPException(status_code=400, detail="Incorrect ids in the database")
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    payload = {"sub": form_data.username, "exp": expire}
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {"access_token": token, "token_type": "bearer"}
